79d14cb58bf85215b5e331e1f6672a8bb8a003d2
yyjia <yyjia@example>
2017-11-26 19:00:33 +0800

    补充文档

---
+++ Algorithm/quickSort.php
@@ -0,0 +1,26 @@
+<?php
+function quickSort(&$arr){
+    if(count($arr)>1){
+        $k=$arr[0];
+        $x=array();
+        $y=array();
+        $_size=count($arr);
+        for($i=1;$i<$_size;$i++){
+            if($arr[$i]<=$k){
+                $x[]=$arr[$i];
+            }elseif($arr[$i]>$k){
+                $y[]=$arr[$i];
+            }
+        }
+        $x=quickSort($x);
+        $y=quickSort($y);
+        return array_merge($x,array($k),$y);
+    }else{
+        return $arr;
+    }
+}
+
+$num = array(3,4,2,1,9,8,1,7,7);
+print_r(quickSort($num));
+
+?>
